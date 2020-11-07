e31b577bde1b01d8b1a1ec5492fff11f859e2bce
Falserok <71407741+Falserok@users.noreply.github.com>
2020-11-07 09:19:58 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,16 +1,54 @@
 public class Main {
+    private static final int SIZE = 10000000;
+    private static final int HALF_SIZE = SIZE / 2;
 
-    public static void main(String[] args) {
+    public float[] calculate(float[] arr) {
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+            }
+        return arr;
+    }
 
-        Sotrudniki[] persArray = new Sotrudniki[5];
-        persArray[0] = new Sotrudniki("Ivanov Ivan", "Engineer", "ukjhfjk@mailbox.com", "8678678", 465456, 12);
-        persArray[1] = new Sotrudniki("Pavlov Vasiliy", "tester", "fhjkhjkfhjk@mailbox.com", "46864868", 3456546, 87);
-        persArray[2] = new Sotrudniki("Dud' Alexandr", "Project lead", "fhjkfhyk@mailbox.com", "468468648", 346, 67);
-        persArray[3] = new Sotrudniki("Yankin Timur", "User", "fhjkfhk@mailbox.com", "64784848468", 3456356, 65);
-        persArray[4] = new Sotrudniki("Vahitov Oleg", "Administrator", "fhjkhfk@mailbox.com", "57895789", 3456346, 34);
+    public void runOneThread() {
+        float[] arr = new float[SIZE];
+        for (int i = 0; i < arr.length; i++) arr[i] = 1.0f;
+        long a = System.currentTimeMillis();
+        calculate(arr);
+        System.out.println("Однопоточный метод заканчивается на: " + (System.currentTimeMillis() - a));
+    }
 
-        for(Sotrudniki item : persArray) {
-            if(item.TakeAge() > 40) item.Show();
-        }
+    public void runTwoThreads() {
+        float[] arr = new float[SIZE];
+        float[] arr1 = new float[HALF_SIZE];
+        float[] arr2 = new float[HALF_SIZE];
+        for (int i = 0; i < arr.length; i++) arr[i] = 1.0f;
+
+        long a = System.currentTimeMillis();
+        System.arraycopy(arr, 0, arr1, 0, HALF_SIZE);
+        System.arraycopy(arr, HALF_SIZE, arr2, 0, HALF_SIZE);
+
+        new Thread() {
+            public void run() {
+                float[] a1 = calculate(arr1);
+                System.arraycopy(a1, 0, arr1, 0, a1.length);
+            }
+        }.start();
+
+        new Thread() {
+            public void run() {
+                float[] a2 = calculate(arr2);
+                System.arraycopy(a2, 0, arr2, 0, a2.length);
+            }
+        }.start();
+
+        System.arraycopy(arr1, 0, arr, 0, HALF_SIZE);
+        System.arraycopy(arr2, 0, arr, HALF_SIZE, HALF_SIZE);
+        System.out.println("Двупоточный метод заканчивается на: " + (System.currentTimeMillis() - a));
+    }
+
+    public static void main(String s[]) {
+        Main o = new Main();
+        o.runOneThread();
+        o.runTwoThreads();
     }
 }
